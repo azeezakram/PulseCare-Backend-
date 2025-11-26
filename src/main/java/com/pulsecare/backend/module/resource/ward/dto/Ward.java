d5e69bab30214c977bfdec6b18780ce340cd9a11
Abdul Azeez <azeez7045@gmail.com>
2025-11-27 03:31:38 +0530

    feat: add Ward entity with fields for id, name, bed count, and timestamps

---
+++ src/main/java/com/pulsecare/backend/module/resource/ward/dto/Ward.java
@@ -0,0 +1,39 @@
+package com.pulsecare.backend.module.resource.ward.dto;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ward")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class Ward {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer bedCount;
+
+    private Integer occupiedBeds;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
