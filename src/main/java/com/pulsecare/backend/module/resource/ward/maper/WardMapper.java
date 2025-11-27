65a850248266381d61ed77293c1d0530f56dd190
Abdul Azeez <azeez7045@gmail.com>
2025-11-27 22:32:22 +0530

    feat(mapper): add updateEntity method to ignore null properties during ward updates

--- src/main/java/com/pulsecare/backend/module/resource/ward/maper/WardMapper.java
+++ src/main/java/com/pulsecare/backend/module/resource/ward/maper/WardMapper.java
@@ -3,12 +3,16 @@ package com.pulsecare.backend.module.resource.ward.maper;
 import com.pulsecare.backend.module.resource.ward.dto.WardReqDTO;
 import com.pulsecare.backend.module.resource.ward.dto.WardResDTO;
 import com.pulsecare.backend.module.resource.ward.model.Ward;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface WardMapper {
     @Mapping(target = "department", ignore = true)
     Ward toEntity(WardReqDTO dto);
     WardResDTO toDTO(Ward obj);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "department", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntity(WardReqDTO dto, @MappingTarget Ward entity);
 }
