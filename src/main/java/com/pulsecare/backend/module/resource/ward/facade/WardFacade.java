ad65e0df400e83356463633add712034ac4526a0
Abdul Azeez <azeez7045@gmail.com>
2025-11-27 05:55:03 +0530

    feat (facade): create ward facade class

---
+++ src/main/java/com/pulsecare/backend/module/resource/ward/facade/WardFacade.java
@@ -0,0 +1,20 @@
+package com.pulsecare.backend.module.resource.ward.facade;
+
+import com.pulsecare.backend.module.resource.ward.dto.WardResDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class WardFacade {
+
+    @Transactional
+    public WardResDTO createWard(WardResDTO dto) {
+        return null;
+    }
+
+    @Transactional
+    public WardResDTO updateWard(WardResDTO dto) {
+        return null;
+    }
+
+}
