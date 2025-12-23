167315e0b721b8b81471d79b183265eb5e5b3658
Abdul Azeez <azeez7045@gmail.com>
2025-12-23 21:39:42 +0530

    feat(service): implement findById method in PatientAdmissionServiceImpl

--- src/main/java/com/pulsecare/backend/module/patient_admission/service/PatientAdmissionServiceImpl.java
+++ src/main/java/com/pulsecare/backend/module/patient_admission/service/PatientAdmissionServiceImpl.java
@@ -1,9 +1,15 @@
 package com.pulsecare.backend.module.patient_admission.service;
 
+import com.pulsecare.backend.common.exception.ResourceNotFoundException;
+import com.pulsecare.backend.module.patient.service.PatientService;
 import com.pulsecare.backend.module.patient_admission.dto.PatientAdmissionReqDTO;
 import com.pulsecare.backend.module.patient_admission.dto.PatientAdmissionResDTO;
+import com.pulsecare.backend.module.patient_admission.mapper.PatientAdmissionMapper;
 import com.pulsecare.backend.module.patient_admission.model.PatientAdmission;
 import com.pulsecare.backend.module.patient_admission.repository.PatientAdmissionRepository;
+import com.pulsecare.backend.module.patient_queue.service.PatientQueueService;
+import com.pulsecare.backend.module.resource.ward.service.WardService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,14 +18,25 @@ import java.util.List;
 public class PatientAdmissionServiceImpl implements PatientAdmissionService {
 
     private final PatientAdmissionRepository repository;
+    private final PatientAdmissionMapper mapper;
+    private final PatientService patientService;
+    private final PatientQueueService patientQueueService;
+    private final WardService wardService;
 
-    public PatientAdmissionServiceImpl(PatientAdmissionRepository repository) {
+    public PatientAdmissionServiceImpl(PatientAdmissionRepository repository, @Qualifier("patientAdmissionMapperImpl") PatientAdmissionMapper mapper,
+                                       PatientService patientService, PatientQueueService patientQueueService, WardService wardService) {
         this.repository = repository;
+        this.mapper = mapper;
+        this.patientService = patientService;
+        this.patientQueueService = patientQueueService;
+        this.wardService = wardService;
     }
 
     @Override
     public PatientAdmissionResDTO findById(Long id) {
-        return null;
+        return mapper.toDTO(
+                repository.findById(id)
+                        .orElseThrow(() ->  new ResourceNotFoundException("Patient admission with id " + id + " not found")));
     }
 
     @Override
