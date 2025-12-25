66a0530353f78c791da78a415bc35d220342784b
Abdul Azeez <azeez7045@gmail.com>
2025-12-26 01:56:14 +0530

    feat(PatientAdmissionRepository): add method to check existence by patient ID and status

--- src/main/java/com/pulsecare/backend/module/patient_admission/repository/PatientAdmissionRepository.java
+++ src/main/java/com/pulsecare/backend/module/patient_admission/repository/PatientAdmissionRepository.java
@@ -1,9 +1,11 @@
 package com.pulsecare.backend.module.patient_admission.repository;
 
+import com.pulsecare.backend.module.patient_admission.enums.PatientAdmissionStatus;
 import com.pulsecare.backend.module.patient_admission.model.PatientAdmission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientAdmissionRepository extends JpaRepository<PatientAdmission, Long> {
+    boolean existsByPatientIdAndStatus(Long patientId, PatientAdmissionStatus status);
 }
